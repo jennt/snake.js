bb7c7692021db6adda65c19add697f8c077d4d4c
Jenn Tustin <jenn.tustin@gmail.com>
2016-12-16 11:58:25 -0500

    Add bigger snake

--- snake.js
+++ snake.js
@@ -4,14 +4,8 @@ var drawSnake = function(snakeToDraw) {
   CHUNK.draw(drawableObjects);
 }
 
-var snake = [{ top: 0, left: 0}]
-drawSnake(snake)
-
-// var snake = [{ top: 0, left: 0 }, {top: 1, left: 1}, {top: 2, left: 1},
-// {top: 3, left: 2} ]
-// var apple = [{ top: 12, left: 15}]
-// var drawableSnake = { color: "magenta", pixels: snake };
-// var drawableApple = { color: "red", pixels: apple };
-// var drawableObjects = [drawableSnake, drawableApple];
-//
-// CHUNK.draw(drawableObjects);
+var snake =[{top:0, left:0}, {top:1, left:1},
+  {top:2, left:2}, {top:2, left:3}, {top:1, left:4},
+  {top:0, left:5}, {top:0, left:6}, {top:0, left:7},
+  {top:1, left:8}, {top:2, left:9}]
+  drawSnake(snake)
