5000d1f6bb4b4cd12889efeebd407887f3bffa4a
Jenn Tustin <jenn.tustin@gmail.com>
2016-12-16 14:17:05 -0500

    Add advanceGame function

--- snake.js
+++ snake.js
@@ -4,8 +4,24 @@ var drawSnake = function(snakeToDraw) {
   CHUNK.draw(drawableObjects);
 }
 
-var snake =[{top:0, left:0}, {top:1, left:1},
-  {top:2, left:2}, {top:2, left:3}, {top:1, left:4},
-  {top:0, left:5}, {top:0, left:6}, {top:0, left:7},
-  {top:1, left:8}, {top:2, left:9}]
+var moveSnake = function(snake) {
+  var oldSegment = snake[0];
+  var newSegment = { top: oldSegment.top, left: oldSegment.left + 1 };
+  var newSnake = [newSegment];
+  return newSnake;
+}
+var advanceGame = function (){
+  snake = moveSnake(snake);
+  drawSnake(snake);
+}
+
+var snake =[{top:0, left:0}]
   drawSnake(snake)
+
+CHUNK.executeNTimesPerSecond(advanceGame, 1);
+
+// var snake = [{top:0, left:0}, {top:1, left:1},
+//   {top:2, left:2}, {top:2, left:3}, {top:1, left:4},
+//   {top:0, left:5}, {top:0, left:6}, {top:0, left:7},
+//   {top:1, left:8}, {top:2, left:9}]
+// drawSnake(snake)
